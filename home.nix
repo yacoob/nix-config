776077fe0602e608f6323ced7d183e63d9cc6685
Jakub Turski <yacoob@ftml.net>
2026-07-13 23:29:28 +0100

    add starship config

--- home.nix
+++ home.nix
@@ -1,20 +1,59 @@
 { config, pkgs, ... }:
 
 {
+  home.stateVersion = "26.05";
+
   home.username = "yacoob";
   home.homeDirectory = "/home/yacoob";
-
-  home.stateVersion = "26.05"; # Please read the comment before changing.
-
-  nixpkgs.config.allowUnfreePredicate = _: true;
-
   home.packages = [];
   home.file = {};
   home.sessionVariables = {};
 
-  programs.home-manager.enable = true;
-  programs.fish.enable = true;
-  programs.starship.enable = true;
-  programs.fd.enable = true;
+  nixpkgs.config.allowUnfreePredicate = _: true;
+
   programs.eza.enable = true;
+  programs.fd.enable = true;
+  programs.fish.enable = true;
+  programs.home-manager.enable = true;
+
+  programs.starship = {
+    enable = true;
+    presets = [ "nerd-font-symbols" ];
+    settings = {
+      format = "$time$custom$line_break$all$line_break$character";
+      cmd_duration.show_milliseconds = true;
+      hostname = {
+        ssh_only = false;
+        format = "[@](red)[$hostname]($style) in ";
+        style = "yellow";
+      };
+      shlvl = {
+        disabled = false;
+        threshold = 5;
+        symbol = " ";
+      };
+      status = {
+        disabled = false;
+        pipestatus = true;
+      };
+      time = {
+        disabled = false;
+        format = "[󱑎 ](red)[$time]($style) ";
+        style = "italic white";
+        time_format = "%T %a %b-%0e";
+      };
+      username = {
+        aliases = { "yacoob" = ""; };
+        show_always = true;
+        format = "[$user]($style)";
+        style_user = "yellow";
+      };
+      kubernetes.disabled = false;
+      battery.display = [
+        { threshold = 15; style = "blink red"; }
+        { threshold = 33; style = "yellow"; }
+        { threshold = 66; style = "dimmed green"; }
+      ];
+    };
+  };
 }
