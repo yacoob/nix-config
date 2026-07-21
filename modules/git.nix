e4417208eba8490605ba22768f0103110844e54f
Jakub Turski <yacoob@ftml.net>
2026-07-21 22:59:07 +0100

    git: set lazygit colors via tokyonight pkg

--- modules/git.nix
+++ modules/git.nix
@@ -1,13 +1,21 @@
 {
   config,
   lib,
+  pkgs,
   vars,
   ...
 }:
 {
-  home.file.".ssh/allowedSigners".text = ''
-    ${vars.email} ${vars.sshKey}
-  '';
+  home = {
+    file.".ssh/allowedSigners".text = ''
+      ${vars.email} ${vars.sshKey}
+    '';
+
+    # lazygit merges its config files; layer tokyonight's theme extra over ours
+    sessionVariables = lib.optionalAttrs (config.flavour.atLeast "base") {
+      LG_CONFIG_FILE = "${pkgs.vimPlugins.tokyonight-nvim}/extras/lazygit/tokyonight_night.yml,${config.xdg.configHome}/lazygit/config.yml";
+    };
+  };
 
   programs = {
     git = {
@@ -104,33 +112,10 @@
 
     lazygit = {
       enable = true;
-      settings = {
-        git = {
-          overrideGpg = true;
-          autoFetch = false;
-        };
-        gui = {
-          nerdFontsVersion = "3";
-          theme = {
-            activeBorderColor = [
-              "#ff9e64"
-              "bold"
-            ];
-            inactiveBorderColor = [ "#27a1b9" ];
-            searchingActiveBorderColor = [
-              "#ff9e64"
-              "bold"
-            ];
-            optionsTextColor = [ "#7aa2f7" ];
-            selectedLineBgColor = [ "#283457" ];
-            cherryPickedCommitFgColor = [ "#7aa2f7" ];
-            cherryPickedCommitBgColor = [ "#bb9af7" ];
-            markedBaseCommitFgColor = [ "#7aa2f7" ];
-            markedBaseCommitBgColor = [ "#e0af68" ];
-            unstagedChangesColor = [ "#db4b4b" ];
-            defaultFgColor = [ "#c0caf5" ];
-          };
-        };
+      # gui block (theme + nerdFontsVersion) comes from tokyonight via LG_CONFIG_FILE
+      settings.git = {
+        overrideGpg = true;
+        autoFetch = false;
       };
     };
   };
