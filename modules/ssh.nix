56a11f6e934c196399a062177645dbd73871ac94
Jakub Turski <yacoob@ftml.net>
2026-07-21 22:59:07 +0100

    ssh: carve out security keys to vars

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -4,8 +4,7 @@
   # ignores keys reached through a store symlink. Keep the store-linked list as
   # authorized_keys_nix and copy it into a real authorized_keys, which inherits ssh_home_t from ~/.ssh.
   home.file.".ssh/authorized_keys_nix".text = ''
-    sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIHhf94VluQZY5JTdsx4JVGJCUQa1LdCXDVAsjHkm3bAkAAAABHNzaDo=
-    sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIFUgnlRzqVppYGS1R5YvCAg/soHTpLh83Pr1lYZ/Ix9LAAAABHNzaDo=
+    ${lib.concatStringsSep "\n" vars.fidoKeys}
     ${vars.sshKey} ${vars.email}
   '';
 
--- vars.nix
+++ vars.nix
@@ -3,6 +3,10 @@
   fullName = "Jakub Turski";
   email = "yacoob@ftml.net";
   sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFn7Jo4qNc92Gh/ZmiD87QTqruvA/3XaaLKWkaFPpZR+";
+  fidoKeys = [
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIHhf94VluQZY5JTdsx4JVGJCUQa1LdCXDVAsjHkm3bAkAAAABHNzaDo="
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIFUgnlRzqVppYGS1R5YvCAg/soHTpLh83Pr1lYZ/Ix9LAAAABHNzaDo="
+  ];
 
   # flavours in ascending order of richness; modules gate on
   # config.flavour.atLeast, see modules/flavour.nix
